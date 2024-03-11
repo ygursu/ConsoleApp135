d55325eee25eb24de14220ec8a960478ae90fde4
ygursu <yasemingrs@gmail.com>
2024-03-11 22:36:50 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp135/Program.cs
@@ -0,0 +1,17 @@
+﻿double sayi1 = 0, sayi2 = 0;
+
+Console.Write("Birinci sayıyı giriniz:");
+sayi1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("İkinci sayıyı giriniz:");
+sayi2 = Convert.ToDouble(Console.ReadLine());
+
+double fark = sayi1 - sayi2;
+
+if (fark > 0)
+    Console.WriteLine("Farkları pozitiftir. Fark={0}", fark);
+else if (fark < 0)
+    Console.WriteLine("Farkları negatiftir. Fark={0}", fark);
+else if (fark == 0)
+    Console.WriteLine("Farkları sıfırdır. Fark={0}", fark);
+
+Console.ReadKey();
